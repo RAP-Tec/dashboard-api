aa82554d57ad0806f1227c64e40bb762ba34b7dc
Rogerio Amaral <rogerio@sistemaaula.com.br>
2025-03-30 09:52:36 -0300

    UPD - Inclusão das querys do cabeçalho do Dashboard

--- src/index.ts
+++ src/index.ts
@@ -23,14 +23,83 @@ app.get('/fetch-data', async (req, res) => {
   try {
     await client.connect();
 
-    // Executa dois SELECTs
-    const result1 = await client.query('SELECT * FROM tags');
-    const result2 = await client.query('SELECT * FROM users');
+    // TOTAL DE CONTATOS E CONTATOS DO MÊS ANTERIOR POR ACCOUNT_ID = 1
+    const result1 = await client.query(`SELECT 
+            COUNT(*) AS total_contacts,
+            (SELECT COUNT(*) 
+            FROM contacts 
+            WHERE account_id = 1 
+            AND created_at >= date_trunc('month', CURRENT_DATE - INTERVAL '1 month') 
+            AND created_at < date_trunc('month', CURRENT_DATE)) AS previous_month_contacts
+        FROM contacts
+        WHERE account_id = 1`);
+
+    // TOTAL DE TEAMS E TEAMS DO MÊS ANTERIOR POR ACCOUNT_ID = 1
+    const result2 = await client.query(`SELECT 
+            COUNT(*) AS total_teams,
+            (SELECT COUNT(*) 
+            FROM teams 
+            WHERE account_id = 1 
+            AND created_at >= date_trunc('month', CURRENT_DATE - INTERVAL '1 month') 
+            AND created_at < date_trunc('month', CURRENT_DATE)) AS previous_month_teams
+        FROM teams
+        WHERE account_id = 1`);
+
+    // TOTAL DE USERS E USERS DO MÊS ANTERIOR POR ACCOUNT_ID = 1
+    const result3 = await client.query(`SELECT 
+            COUNT(*) AS total_users,
+            (SELECT COUNT(*) 
+            FROM account_users 
+            WHERE account_id = 1 
+            AND created_at >= date_trunc('month', CURRENT_DATE - INTERVAL '1 month') 
+            AND created_at < date_trunc('month', CURRENT_DATE)) AS previous_month_users
+        FROM account_users
+        WHERE account_id = 1`);
+
+    // TOTAL DE conversations E conversations DO MÊS ANTERIOR POR ACCOUNT_ID = 1
+    const result4 = await client.query(`SELECT 
+          COUNT(*) AS total_conversations,
+            (SELECT COUNT(*) 
+            FROM conversations 
+            WHERE account_id = 1 
+            AND last_activity_at >= date_trunc('month', CURRENT_DATE - INTERVAL '1 month') 
+            AND last_activity_at < date_trunc('month', CURRENT_DATE)) AS previous_month_conversations
+        FROM conversations
+        WHERE account_id = 1`);
+
+    // TOTAL DE conversations E conversations DO MÊS ANTERIOR POR ACCOUNT_ID = 1
+    const result5 = await client.query(`SELECT 
+          COUNT(*) AS total_today_conversations,
+            (SELECT COUNT(*) 
+            FROM conversations 
+            WHERE account_id = 1 
+            AND last_activity_at >= CURRENT_DATE 
+            AND last_activity_at < CURRENT_DATE + INTERVAL '1 day') AS total_yesterday_conversations
+        FROM conversations
+        WHERE account_id = 1
+        AND last_activity_at >= CURRENT_DATE - INTERVAL '1 day'
+        AND last_activity_at < CURRENT_DATE;`);
+    
+    // TOTAL DE messages E messages DO MÊS ANTERIOR POR ACCOUNT_ID = 1
+    const result6 = await client.query(`SELECT 
+          COUNT(*) AS total_messages,
+            (SELECT COUNT(*) 
+            FROM messages 
+            WHERE account_id = 1 
+            AND created_at >= date_trunc('month', CURRENT_DATE - INTERVAL '1 month') 
+            AND created_at < date_trunc('month', CURRENT_DATE)) AS previous_month_messages
+        FROM messages
+        WHERE account_id = 1;`);
+    
 
     // Estrutura os resultados
     const data = {
       tabela1: result1.rows,
       tabela2: result2.rows,
+      tabela3: result3.rows,
+      tabela4: result4.rows,
+      tabela5: result5.rows,
+      tabela6: result6.rows,
     };
 
     // Retorna os dados na resposta HTTP
